1d789026772bc4ad4bcdf93cb9f2e9957c563dfb
CatOctopus <chalkywilston@gmail.com>
2014-10-21 19:33:13 -0400

    Chapter 5 Final Reading Response

---
+++ Chapter 5 Final Reading Response.py
@@ -0,0 +1,10 @@
+"""
+5.8 & 5.9
+
+The int function is often better to use than eval for converting strings to numbers, as eval cannot
+operate on any number with leading zeros.  { } brackets can be used to provide formating instruction
+for strings. < > ^ can be used in formating commands to specify justification of the formated input.
+Python can open text files with the open command, and those files can be read from or writen into.
+Python will, however, delete any files opened with the write command and make a new one in its place.
+
+"""
